d919efebbfa3e7fe5754b2e61b6fe8f06d3307e2
hustfc <spfanc@gmail.com>
2019-09-03 17:50:04 +0800

    leetcode78

--- src/com/leetcode/solution/Solution78.java
+++ src/com/leetcode/solution/Solution78.java
@@ -1,4 +1,7 @@
 package com.leetcode.solution;
+
+import sun.awt.image.ImageWatched;
+
 import java.util.*;
 
 /**
@@ -7,21 +10,28 @@ import java.util.*;
  * 78子集
  */
 public class Solution78 {
+
+    List<List<Integer>> output = new LinkedList<>();
+    int[] nums;
+
     /**
      * 回溯函数
-     * @param index 元素索引
-     * @param nums 数组
+     * @param index   元素索引
+     * @param current 当前结果
      */
-    public void traceback(int index, int[] nums) {
-
+    public void traceback(int index, LinkedList<Integer> current) {
+        output.add(new LinkedList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            traceback(i + 1, current);
+            current.removeLast();
+        }
     }
 
 
     public List<List<Integer>> subsets(int[] nums) {
-        if (nums.length == 0) {
-            return new LinkedList<>(new LinkedList<>());
-        }
-
-
+        this.nums = nums;
+        traceback(0, new LinkedList<>());
+        return output;
     }
 }
---
+++ src/test/Test78.java
@@ -0,0 +1,14 @@
+package test;
+
+import com.leetcode.solution.Solution78;
+
+/**
+ * @Author : fanc
+ * @Date : 2019-09-03 17:42
+ */
+public class Test78 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(new Solution78().subsets(nums));
+    }
+}
