a34953fa4029b84d9b49e2f4b8831800a87b736f
hustfc <spfanc@gmail.com>
2020-06-18 22:25:00 +0800

    6.18

---
+++ src/com/leetcode/solution/Solution221.java
@@ -0,0 +1,11 @@
+package com.leetcode.solution;
+
+/**
+ * @Author : fanc
+ * @Date : 2020/6/18 4:27 下午
+ */
+public class Solution221 {
+    public int maximalSquare(char[][] matrix) {
+        return 1;
+    }
+}
